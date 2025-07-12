b5d98b2862111de9114a00da90d9ad1e552d7bca
Jonathan <jonathan.a.farias@gmail.com>
2025-07-12 11:44:08 -0300

    feat: tuplas examples added

---
+++ python/estrutura-de-dados/tuplas.py
@@ -0,0 +1,9 @@
+#imutável, diferente da lista
+
+frutas = ("laranja","pera","uva",)
+letras = tuple("python")
+
+print(frutas[1])
+print(frutas[-1])
+print(len(frutas))
+print(frutas.index("laranja"))
